fadcf5cd036c83168e11c9d669e2f10625bbbd5d
kartoniks <arturderechowski@gmail.com>
2017-10-28 20:00:39 +0200

    Add files via upload

--- lista4/zad3.py
+++ lista4/zad3.py
@@ -18,6 +18,25 @@ def znajdz(n,s):
     if happy(i,s) and prime(i):
       return i
 
+def genhap(n):
+  L=[]
+  for i in range(10):
+    for j in range(10):
+      for k in range(10):
+          L.append(7777777000+100*i+10*j+k)
+          L.append(i*10**9+777777700+10*j+k)
+          L.append(i*10**9+j*10**8+77777770+k)
+          L.append(i*10**9+j*10**8+k*10**7+7777777)
+  L.sort()
+  L=set(L)
+  return L
 
 
-print(znajdz(10,7))
+L=genhap(10)
+p=0
+for i in L:
+  if prime(int(i)):
+    p+=1
+
+print(p)
+
